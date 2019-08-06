cd5815f4c3ad4189198f076f333b5e2eb13b5d28
Артем Веркеев <a.verkeev@whatwhere.world>
2019-08-06 20:59:33 +0300

    up to 0.0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-anti-profanity',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(exclude=('demo', )),
     install_requires=[
         "Django>=2.0",
